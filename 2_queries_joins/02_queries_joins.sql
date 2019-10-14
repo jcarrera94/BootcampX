356cb3c6cd94441a54f90beb27dd43ffed6be10c
jcarrera94 <juankarrera@gmail.com>
2019-10-14 22:21:42 +0000

    Implemented the group by and having functions in our database

--- 2_queries_joins/02_queries_joins.sql
+++ 2_queries_joins/02_queries_joins.sql
@@ -16,3 +16,9 @@ JOIN assignment_submissions ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE 'FEB12';
 
+SELECT students.name as student, COUNT(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON student_id = students.id
+WHERE students.end_date IS NULL
+GROUP BY student
+HAVING COUNT(assignment_submissions.*) < 100;
