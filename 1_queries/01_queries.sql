af58b347e89aa9578d813cf0fd5ec05abc9383a2
jcarrera94 <juankarrera@gmail.com>
2019-10-14 19:01:55 +0000

    First round of queries for the bootcampx project included: Students in Cohort, Total Students in Cohorts, Contact Details, Non Gmail Students, Currently ENrolled students and GitHub Activity

---
+++ 1_queries/01_queries.sql
@@ -0,0 +1,29 @@
+SELECT students.id, name 
+FROM students
+WHERE cohort_id = 1
+ORDER BY name;
+
+SELECT COUNT(*)
+FROM students
+WHERE cohort_id IN (1,2,3); 
+
+SELECT name, id, cohort_id
+FROM students 
+WHERE email IS NULL
+OR phone IS NULL;
+
+SELECT name, email, id, cohort_id
+FROM students
+WHERE email NOT LIKE '%gmail%'
+AND phone IS NULL;
+
+SELECT name, id, cohort_id
+FROM students
+WHERE end_date IS NULL
+ORDER BY cohort_id;
+
+SELECT name, email, phone
+FROM students
+WHERE end_date IS NOT NULL
+AND github IS NULL
+ORDER BY cohort_id;
